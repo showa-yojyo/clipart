5ec419a0cf10172ec62abce214c800372ad41084
showa_yojyo <yojyo@hotmail.com>
2015-12-29 23:31:11 +0900

    (rainbow.py) Use numpy.linspace to make linear gradient.

--- tools/rainbow.py
+++ tools/rainbow.py
@@ -1,40 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""rainbow.py
+"""rainbow.py: Generate code snippet of an Inkscape SVG document
+which defines a linear gradient of rainbow spectrum, from red to
+violet.
 
 Usage:
-  rainbow.py
+  rainbow.py [--help] [--version]
+  rainbow.py [-s | --saturation {0.0:1.0}] [-l | --lightness {0.0:1.0}]
 """
 
+from argparse import ArgumentParser
 from colorsys import hsv_to_rgb
+import numpy as np
 
-__version__ = '0.0.0'
+__version__ = '1.0.0'
 
+# Snippet template of nodes between <linearGradient> and </linearGradient>.
 stop_template = '''\
       <stop
          id="stop-rainbow-{id:02d}"
          offset="{offset}"
          style="stop-color:#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x};stop-opacity:1" />'''
 
+def configure():
+    """Parse the command line parameters.
+
+    Returns:
+        An instance of argparse.ArgumentParser that stores the command line
+        parameters.
+    """
+
+    parser = ArgumentParser()
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument(
+        '-i', '--interval',
+        type=int,
+        default=16,
+        help='number of intervals')
+    parser.add_argument(
+        '-s', '--saturation',
+        type=float,
+        default=1.0,
+        metavar='{0.0:1.0}',
+        help='saturation of HSL')
+    parser.add_argument(
+        '-l', '--lightness', '--value', '--brightness',
+        dest='lightness',
+        type=float,
+        default=1.0,
+        metavar='{0.0:1.0}',
+        help='lightness (a.k.a. value or brightness) of HSL')
+
+    return parser
+
 def main():
-    s, v = 1.0, 1.0
-    num_div = 16
-    kwargs = {}
+    """The main function."""
 
-    for i in range(num_div):
-        offset = 1.0 * i / num_div
-        kwargs['offset'] = offset
-        kwargs['id'] = i
-        kwargs['rgb'] = [round(j * 0xFF) for j in hsv_to_rgb(offset, s, v)]
-        print(stop_template.format(**kwargs))
+    args = configure().parse_args()
+    s, v = args.saturation, args.lightness
 
-    kwargs['offset'] = 1
-    kwargs['id'] = num_div
-    r, g, b = (round(j * 0xFF) for j in hsv_to_rgb(1.0, s, v))
-    kwargs['r'] = r
-    kwargs['g'] = g
-    kwargs['b'] = b
-    print(stop_template.format(**kwargs))
+    nodes = np.linspace(0, 1, args.interval + 1)
+
+    colors = np.array([hsv_to_rgb(h, s, v) for h in nodes])
+    colors *= 255
+    colors = colors.astype(int)
+
+    for (i, (offset, rgb)) in enumerate(zip(nodes, colors)):
+        print(stop_template.format(
+            offset=offset, id=i, rgb=rgb))
 
 if __name__ == '__main__':
     main()
